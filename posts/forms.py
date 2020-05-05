3154b82edbfbe74b0b60e1ba3de24edb5dc516e8
chravindra414 <chravindra414@gmail.com>
2020-05-05 14:17:30 -0500

    Added validation using Model forms

--- posts/forms.py
+++ posts/forms.py
@@ -17,3 +17,18 @@ class PostModelForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
+    # Validating the fields
+
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get('title')
+        qs = Post.objects.filter(title__iexact=title)
+        if qs.exists():
+            raise forms.ValidationError(
+                'Title has been already used. Please enter new one')
+        return title
+
+    def clean_content(self, *args, **kwargs):
+        content = self.cleaned_data.get('content')
+        if len(content) > 200:
+            raise forms.ValidationError('Content is too long to save')
+        return content
--- posts/models.py
+++ posts/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    content = models.TextField(max_length=250)
+    content = models.TextField()
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
 
